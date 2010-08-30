6fb5ee8a91eb402bfa3d3fcd33376511e98e70ee
Jay McCarthy <jay.mccarthy@gmail.com>
2010-08-30 14:51:27 -0600

    up

--- info.rkt
+++ info.rkt
@@ -7,7 +7,7 @@
 (define blurb
   (list "A native Racket interface to memcached"))
 (define scribblings '(("memcached.scrbl" ())))
-(define primary-file "main.ss")
+(define primary-file "main.rkt")
 (define categories '(net io))
 (define compile-omit-paths
   (list "test.ss"))
